4e4da60c2647de8171aaed942b568a890067824c
U-ATS-AR\cescobar <cescobar@PC-CESCOBAR.ats-ar.com.ar>
2018-10-18 13:54:21 -0300

    Fin Fuerza Bruta

--- tp2/p1/safepath.py
+++ tp2/p1/safepath.py
@@ -9,6 +9,7 @@ from collections import deque, namedtuple
 
 Edge = namedtuple('Edge', 'start, end, distance')
 Coordinate = namedtuple('Coordinate', 'x,y')
+WeightedPath = namedtuple('WeightedPath', 'path,weight')
 
 def make_edge(start, end, distance=1):
   return Edge(start, end, distance)
@@ -19,11 +20,9 @@ class Graph():
 		self.edges = edges
 		
 	def add_edge(self, n1, n2, distance=1):
-		#print("add_edge [%s] [%s] [%s]" % (n1,n2,distance))
 		self.edges.append(Edge(start=n1, end=n2, distance=distance))
 
 	def add_edge2(self, edge):
-		#print("add_edge [%s] [%s] [%s]" % (edge))
 		self.edges.append(edge)
 			
 	def remove_edge(self, edge):
@@ -89,12 +88,14 @@ def distance(p1, p2):
 	return math.sqrt(pow(p1.x-p2.x, 2) + pow(p1.y-p2.y, 2))
 	
 def fuerza_bruta(s, t, safe_points):
-	print("fuerzabruta [%s] [%s] [%s]" % (s, t, safe_points))
+	#print("fuerzabruta [%s] [%s] [%s]" % (s, t, safe_points))
 	
 	minimum_convex_hull = []
 	s_present = False
 	t_present = False
-		
+	
+	coordinates_dict = { key: [] for key in safe_points}
+			
 	for i in range(0, len(safe_points)):
 		for j in range(0, len(safe_points)):
 			if i == j:
@@ -116,6 +117,11 @@ def fuerza_bruta(s, t, safe_points):
 			
 			if allPointsOnTheRight:
 				e = Edge(start=pointI, end=pointJ, distance=distance(pointI, pointJ))
+								
+				if pointJ not in coordinates_dict[pointI] and safe_points[i] != pointJ:
+					coordinates_dict[pointI].append(pointJ)
+				if pointI not in coordinates_dict[pointJ] and safe_points[j] != pointI:
+					coordinates_dict[pointJ].append(pointI)
 				
 				if pointI == s or pointJ == s:
 					s_present = True
@@ -126,11 +132,48 @@ def fuerza_bruta(s, t, safe_points):
 				if e not in minimum_convex_hull:
 					minimum_convex_hull.append(e)
 	
+	path1 = []
+	path2 = []
+	weight_1 = 0
+	weight_2 = 0
+	
+	path1.append(s)
+	path1.append(coordinates_dict[s][0])
+	path2.append(s)
+	path2.append(coordinates_dict[s][1])
+	
+	reached_t = False
+	
+	while not reached_t:
+		last_element = path1[len(path1)-1]
+		next = coordinates_dict[last_element]
+		
+		for adj in next:
+			if adj not in path1:
+				path1.append(adj)
+				weight_1 += distance(last_element, adj)
+				
+			if adj == t:
+				reached_t = True
+				
+	reached_t = False
+	while not reached_t:
+		last_element = path2[len(path2)-1]
+		next = coordinates_dict[last_element]
+		
+		for adj in next:
+			if adj not in path2:
+				path2.append(adj)
+				weight_2 += distance(last_element, adj)
+				
+			if adj == t:
+				reached_t = True
+				
 	if not t_present or not s_present:
 		print("ERROR: Convex Hull does not contain source or tail.")
 		sys.exit(500)	
 	
-	return minimum_convex_hull
+	return (WeightedPath(path=path1,weight=weight_1), WeightedPath(path=path2,weight=weight_2))
 	
 def graham():
 	print("graham")
@@ -148,8 +191,8 @@ mode=sys.argv[2]
 #
 #
 convex_hull = []
-path1 = []
-path2 = []
+path1 = None
+path2 = None
 s = ()
 t = ()
 safe_points = []
@@ -165,14 +208,14 @@ s = safe_points[0]
 t = safe_points[1]
 
 if mode == 'F':
-	convex_hull = fuerza_bruta(s, t, safe_points)		
+	path1,path2 = fuerza_bruta(s, t, safe_points)		
 		
-	g = Graph([])
-    
-	for edge in convex_hull:
-		g.add_edge2(edge)
-				
-	path1=g.dijkstra(s, t)
+	#g = Graph([])
+    #
+	#for edge in convex_hull:
+	#	g.add_edge2(edge)
+	#			
+	#path1=g.dijkstra(s, t)
 
 elif mode == 'G':
 	graham()
@@ -181,16 +224,11 @@ elif mode == 'D':
 else:
 	print("Method [%s] is not valid." % mode)
 	os._exit(-1)
-
-#print("###################")	
-#print("Initial Point [%s], Final Point [%s]" % (s,t))
-#print("Safe Points %s" % safe_points)
 	
-print("###################\n\n")
-print("Camino 1: Longitud [%s]" % len(path1))
-print("Recorrido: " + str(path1))
+print("Camino 1: Longitud [%s]" % path1.weight)
+print("Recorrido: " + str(path1.path))
 
-print("Camino 2: Longitud [%s]" % len(path2))
-print("Recorrido: " + str(path2))
+print("Camino 2: Longitud [%s]" % path2.weight)
+print("Recorrido: " + str(path2.path))
 
-print("Camino seleccionado: [%s]" % min(len(path1), len(path2)) )
+print("Camino seleccionado: [%s]" % (1 if path1.weight < path2.weight else 2))
